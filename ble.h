ba35ec74f5498684cc87bc80c7e901174c7d6163
ASanz <asanz>
2022-11-18 08:23:29 +0100

    APIs renaming, initial modification

--- ble.h
+++ ble.h
@@ -7,6 +7,9 @@ void ble_Setup(void);
 
 void ble_startAdvertising(void);
 
+
+bool ble_isConnected(void);
+
 /*
  * Set up the power service
  */
@@ -22,12 +25,12 @@ void ble_PublishPower(int16_t instantPwr, uint16_t crankRevs, long millisLast);
 /*
  * Publish the battery status measurement.
  */
-void blePublishBatt(uint8_t battPercent);
+void ble_PublishBatt(uint8_t battPercent);
 
 /*
  * Publish a tiny little log message over BLE. Pass a null-terminated
  * char*, in 20 chars or less (counting the null).
  */
-void blePublishLog(const char* fmt, ...);
+void ble_PublishLog(const char* fmt, ...);
 
 #endif
